aa7ffdf388e6b75271445e6be442b1ca2ce627f6
aKacharava <a.kacharava94@gmail.com>
2023-06-11 12:01:40 +0200

    Pass on data from laravel to vue using inertia

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,8 +1,18 @@
 <template>
   <div>Index</div>
   <Link href="/hello">Show page</Link>
+
+  <div>
+    The message: {{ message }}
+  </div>
 </template>
 
 <script setup>
   import { Link } from '@inertiajs/vue3'
+
+  defineProps({
+    message: {
+      type: String
+    }
+  })
 </script>
