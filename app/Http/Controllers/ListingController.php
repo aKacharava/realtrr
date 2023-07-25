c41756863ad983a5e86d40c04c2a1cf48f10aec1
Alexander Kacharava <alexander@coddin.nl>
2023-07-25 13:25:57 +0200

    Form submission setup frontend

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Listing;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ListingController extends Controller
 {
@@ -22,7 +23,7 @@ class ListingController extends Controller
      */
     public function create()
     {
-        //
+        return Inertia('Listing/Create');
     }
 
     /**
