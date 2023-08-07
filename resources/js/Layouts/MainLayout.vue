642a3f80055c3dc845f015e74762161091649760
Alexander Kacharava <alexander@coddin.nl>
2023-08-07 10:20:20 +0200

    Add data validation and feedback from backend

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,9 +1,22 @@
 <template>
     <Link href="/">Index page</Link>&nbsp;
     <Link href="/hello">Show page</Link>
+    <div v-if="flashSuccess" class="success">
+        {{ flashSuccess }}
+    </div>
     <slot></slot>
 </template>
 
 <script setup>
-import { Link } from "@inertiajs/vue3";
+import { computed } from "vue";
+import { Link, usePage } from "@inertiajs/vue3";
+
+const flashSuccess = computed(() => usePage().props.flash.success);
 </script>
+
+<style scoped>
+.success {
+    background: purple;
+    color: white;
+}
+</style>
